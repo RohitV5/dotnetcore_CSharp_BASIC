7dafd740ff876f8332ae5b9f66810019286b93e8
Rohit Verma <verma.rohit.in@gmail.com>
2020-01-31 23:00:25 +0530

    first push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
